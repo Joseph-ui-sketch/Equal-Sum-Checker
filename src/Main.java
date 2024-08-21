cca71a90170a27a59bc3897ed4846228cf1fb071
Joseph-ui-sketch <hernajos000@warren.k12.in.us>
2024-08-21 13:21:42 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String [] arg) {
+        hasEqualSum(1,1,1);
+        hasEqualSum(1,1,2);
+        hasEqualSum(1,-1,0);
+
+    }
+        public static boolean hasEqualSum(int num1, int mun2, int num3){
+         //find the sum num1 and num2 store it as a variable
+         // return whether that sum is equal to num3
+    }
+}
